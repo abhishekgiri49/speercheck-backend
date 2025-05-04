e88d685bb814820e343e5d94951c7cfc40a6f71f
abhishekgiri49 <abhishekgiri49@hotmail.com>
2025-05-04 16:07:47 -0400

    first

--- config/database.js
+++ config/database.js
@@ -1,8 +1,10 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('basic', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
-
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize(
+  'postgres://postgres.dmmgjpprllyycpmwgsyk:xKDRbmcnYcFynLky@aws-0-us-east-1.pooler.supabase.com:6543/postgres',
+  {
+    dialectOptions: {
+      ssl: { require: true, rejectUnauthorized: false }
+    }
+  }
+);
 module.exports = sequelize;
